dd877b715959635e3235cff0e9b6c4e52b2d4229
Your Name <omerkumble@gmail.com>
2026-06-11 11:34:21 -0700

    new commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.keys import Keys
 EMAIL = "iamcravingagummybear@test.com"
 PASSWORD = "Zaid@12?"
 
-# ---- keeps browzer open ---- #
+# ---- keeps browser open ---- #
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
@@ -47,30 +47,69 @@ time.sleep(5)
 # ---- finds the next thursday date ---- #
 today = datetime.date.today()
 days_ahead = (3 - today.weekday()) % 7
-if days_ahead == 0:
-    days_ahead = 7
+
 
 next_thursday = today + datetime.timedelta(days=days_ahead)
 thursday_month = next_thursday.strftime("%b").lower()
 thursday_day = next_thursday.day
 
+if thursday_day < 10:
+    thursday_day_format = f"0{thursday_day}"
+else:
+    thursday_day_format = thursday_day
+
+
+thursday_month_format = datetime.datetime.strptime(thursday_month, "%b").strftime("%m")
 day_container_id = f"day-group-thu-{thursday_month}-{thursday_day}"
 
 # ---- finds thursday tab ---- #
+
+booked = 0
+waitlisted = 0
+already_booked = 0
+
 try:
     thursday_tab = driver.find_element(By.CSS_SELECTOR,
                                        f"a[href='#{day_container_id}'], button[data-target='#{day_container_id}'], [id*='thu']")
+
     thursday_tab.click()
     time.sleep(2)
+
     # ---- finds the 6:00 events in thurday ---- #
-    book_button = driver.find_element(By.CSS_SELECTOR, "button[id$='2026-06-04-1800']")
+    book_button = driver.find_element(By.CSS_SELECTOR, f"button[id$='2026-{thursday_month_format}-{thursday_day_format}-1800']")
+
     # ---- books it ---- #
-    driver.execute_script("arguments[0].click();", book_button)
-    print("Successfully clicked the 6:00 PM Book Class button!")
-except Exception as e:
+    if  book_button.text == "Booked":
+        print(f"Already booked: Spin Class on Thursday, {thursday_month}, {thursday_day}")
+        already_booked += 1
+
+
+    elif book_button.text == "Waitlisted":
+        print(f"Already on waitlist: Spin Class on Thursday, {thursday_month}, {thursday_day}")
+        already_booked += 1
+
+    elif book_button.text == "Join Waitlist":
+        driver.execute_script("arguments[0].click();", book_button)
+        print(f"Joined waitlist for: Spin Class on Thursday, {thursday_month}, {thursday_day}")
+        waitlisted += 1
+
+    elif  book_button.text == "Book Class":
+        driver.execute_script("arguments[0].click();", book_button)
+        print(f"Joined waitlist for: Spin Class on Thursday, {thursday_month}, {thursday_day}")
+        booked += 1
+
     # ---- if no 6:00 events on thursday ---- #
-    print("Could not find a 6:00 PM class button for this Thursday.", e)
+except Exception as e:
+    print(f"Could not find a 6:00 PM class button for {thursday_month} : {thursday_day}.", e)
+
 
 
 my_bookings = driver.find_element(By.ID, "my-bookings-link")
 my_bookings.click()
+
+# ---- SUMMARY ---- #
+
+print(f"--- BOOKING SUMMARY ---\nClasses booked: {booked}\nWaitlists joined: {waitlisted}\nAlready booked/waitlisted: {already_booked}\nTotal Tuesday 6pm classes processed: {already_booked + booked + waitlisted}")
+
+
+
